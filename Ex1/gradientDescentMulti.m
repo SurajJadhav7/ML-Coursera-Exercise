6fe52dcb2aa33fe0a112aadfd178992e06d0f01a
Suraj Jadhav <49967508+SurajJadhav7@users.noreply.github.com>
2019-05-19 19:07:27 +0530

    Update gradientDescentMulti.m

--- Ex1/gradientDescentMulti.m
+++ Ex1/gradientDescentMulti.m
@@ -1,7 +1,6 @@
 function [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters)
 m = length(y);
 J_history = zeros(num_iters, 1);
-
 for iter = 1:num_iters
     temp=zeros(size(X,2),1);
     for i = 1:size(X,2)
