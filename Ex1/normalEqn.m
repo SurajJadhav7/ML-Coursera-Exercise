906a6af20b135d9d7b869898e0db914154cac89c
Suraj Jadhav <49967508+SurajJadhav7@users.noreply.github.com>
2019-05-19 19:05:58 +0530

    Update normalEqn.m

--- Ex1/normalEqn.m
+++ Ex1/normalEqn.m
@@ -8,16 +8,7 @@ theta = zeros(size(X, 2), 1);
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the code to compute the closed form solution
 %               to linear regression and put the result in theta.
-%
-
-% ---------------------- Sample Solution ----------------------
-
 theta=((X'*X)^-1)*X'*y
-
-
-% -------------------------------------------------------------
-
-
 % ============================================================
 
 end
